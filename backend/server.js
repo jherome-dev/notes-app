361cada469efb548d73be3d7e42253769465dbb6
jherome-dev <83631557+jherome-dev@users.noreply.github.com>
2024-11-29 16:39:21 +0800

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -17,7 +17,7 @@ app.use(express.json());
 
 app.use(
   cors({
-    origin: process.env.FRONTEND_URL || "http://localhost:5173",
+    origin: process.env.FRONTEND_URL,
     methods: ["GET", "POST", "PUT", "DELETE"],
     credentials: true,
   })
