3b1ecec2fa2fe040f6f6a8f6f90190bc1ae80441
jherome-dev <83631557+jherome-dev@users.noreply.github.com>
2024-11-30 18:17:22 +0800

    update passpot js

--- backend/config/passport.js
+++ backend/config/passport.js
@@ -17,7 +17,7 @@ passport.use(
     {
       clientID: process.env.GOOGLE_CLIENT_ID,
       clientSecret: process.env.GOOGLE_CLIENT_SECRET,
-      callbackURL: `${process.env.FRONTEND_URL}/auth/google/callback`,
+      callbackURL: `${process.env.BACKEND_URL}/auth/google/callback`,
     },
     async (accessToken, refreshToken, profile, done) => {
       const { id, displayName, emails } = profile;
